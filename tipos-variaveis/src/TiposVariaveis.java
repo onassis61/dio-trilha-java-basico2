8518c4b62bd67c3ef30308e53b6154d5fc3b5035
OnassisKz <onassisandrade61@gmail.com>
2023-12-14 15:57:33 -0300

    conclusao tipos e variaveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,27 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
         double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+       
+        String meuNome = "Nome e Sobrenome";
+
+            System.out.println(meuNome);
+
+        int numero = 5;
+        int numero2 = 10;
+        int numero3 = 19;
+        System.out.print(numero + numero2 * numero3);
+
+        //Adicionando final o valor da variavel nao pode ser alterada
+        final double VALOR_DE_PI = 3.14;
+        //VALOR_DE_PI = 10.75;
+
+        //Revisar tipos primitivos e classe String que representa texto.
     }
 }
+
+
